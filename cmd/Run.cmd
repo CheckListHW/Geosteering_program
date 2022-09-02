034fd26b2acb84b4b664fda8e1add51febe0ccca
Иван Косачев <kosach_1@live.com>
2022-09-02 14:23:42 +0700

    ref

--- cmd/Run.cmd
+++ cmd/Run.cmd
@@ -1,4 +1,3 @@
-if not exist Geosteering\venv FirstLaunch.cmd
+if not exist Geosteering\venv call Geosteering\cmd\FirstLaunch.cmd
 call Geosteering\venv\Scripts\activate
-python Geosteering\main.py --scenario_path=Data\input.xml --gr_path=Data\input.las --segments_count=100 --delta_deg=10 --st=0.5 --metric=r2 --result_path=Data\output.xml
-call Geosteering\venv\Scripts\deactivate
+python -W ignore Geosteering\main.py --scenario_path=Data\input.xml --gr_path=Data\input.las --segments_count=100 --delta_deg=10 --st=0.5 --metric=r2 --result_path=Data\output.xml
--- geosteering_tools/tools.py
+++ geosteering_tools/tools.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 from numpy.fft import rfft, rfftfreq, irfft
 # from skfda.misc.metrics import LpDistance
