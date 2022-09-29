79f9cd9584b720d3837e6145c5769a319ba60aac
Иван Косачев <kosach_1@live.com>
2022-09-29 19:08:48 +0700

    edit save

--- Geosteering/test.cmd
+++
@@ -1,4 +0,0 @@
-set dirData =%cd%\Data\
-
-call project\venv\Scripts\activate
-call python project\main.py --scenario_path="%dirData %input.xml" --offset_path="%dirData %input.las" --result_path="%dirData %output.xml"
