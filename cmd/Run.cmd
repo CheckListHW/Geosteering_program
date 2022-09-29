a8b51baa231be114ca4a3ebec0cad295ab173da1
Иван Косачев <kosach_1@live.com>
2022-09-29 17:39:21 +0700

    clean

--- cmd/Run.cmd
+++
@@ -1,7 +0,0 @@
-@echo OFF
-if not exist project\venv (
-	call install.cmd
-)
-call project\venv\Scripts\activate
-call python -W ignore project\main.py
-if exist project\output\result.xml call copy project\output\result.xml Data\output.xml /Y
--- cmd/install.cmd
+++
@@ -1,4 +0,0 @@
-call project\python\python.exe -m venv project\venv
-call project\venv\Scripts\Activate
-call python.exe -m pip install --upgrade pip
-call pip install -r project\requirements.txt
